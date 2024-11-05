943d1e153efd24aabc69a11825f6c83fa28b5d5c
Ifunanya Obinabo <iobinabo247@gmail.com>
2024-11-04 19:40:56 -0600

    Test 2 for TarCode

---
+++ autonomous_testing.py
@@ -0,0 +1,40 @@
+import mavsdk
+import asyncio
+
+
+async def main():
+
+    drone = mavsdk.System() ## MAVSDK object that represents drone
+
+    await drone.connect(system_address="udp://:14540") ## default system address of SITL
+
+    print("Waiting for drone to connect...")
+    async for state in drone.core.connection_state():
+        if state.is_connected:
+            print("-- Connected to drone!")
+            break
+
+    # arm the drone
+
+    print("Arming...")
+    await drone.action.arm()
+
+    # finally, takeoff! 
+
+    print("Taking off!")
+    await drone.action.takeoff()
+
+    await asyncio.sleep(20) # wait a while
+    
+
+    
+    print("Landing!")
+    await drone.action.land() # finally, land the drone
+
+
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+
